f856c5ff3622f9990891f80230d91b1b388048bc
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2025-06-01 19:36:46 -0700

    moving remoteUser name to json file

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -4,16 +4,8 @@ set -e
 echo "Enter a password for root user:"
 sudo passwd root
 
-echo "Enter a password for user vscode:"
-sudo passwd vscode
-
-# setup user name and pw
-DEVPOD_USER="devpod-user"
-if id -u vscode >/dev/null 2>&1; then
-  echo "Renaming user 'vscode' to '$DEVPOD_USER'..."
-  sudo usermod -l "$DEVPOD_USER" vscode
-  sudo groupmod -n "$DEVPOD_USER" vscode
-fi
+echo "Enter a password for devpod-user:"
+sudo passwd devpod-user 
 
 # setup zsh shell
 echo "Setting up zshrc and switching to zsh..."
