203ebee912532a1342860abee3781a5e2d8b8092
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2025-06-01 19:27:43 -0700

    order for system setup

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "Enter a password for root user:"
+sudo passwd root
+
+echo "Enter a password for user vscode:"
+sudo passwd vscode
+
 # setup user name and pw
 DEVPOD_USER="devpod-user"
 if id -u vscode >/dev/null 2>&1; then
@@ -8,12 +14,6 @@ if id -u vscode >/dev/null 2>&1; then
   sudo usermod -l "$DEVPOD_USER" vscode
   sudo groupmod -n "$DEVPOD_USER" vscode
 fi
-
-echo "Enter a password for root user:"
-sudo passwd root
-
-echo "Enter a password for user $DEVPOD_USER:"
-sudo passwd "$DEVPOD_USER"
 
 # setup zsh shell
 echo "Setting up zshrc and switching to zsh..."
