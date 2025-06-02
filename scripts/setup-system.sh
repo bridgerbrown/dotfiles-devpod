6e1481db8daae722f239000b8c313ae5d828a9f7
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2025-06-01 20:30:40 -0700

    brew fix

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -9,10 +9,10 @@ echo "Enter a password for user vscode:"
 sudo passwd vscode
 
 # .bashrc
-echo "Setting up '.bashrc'..."
-if [ -f ~/.bashrc ]; then
-  source ~/.bashrc
-fi
+# echo "Setting up '.bashrc'..."
+# if [ -f ~/.bashrc ]; then
+#   source ~/.bashrc
+# fi
 
 echo -e "\n\n"
 echo -e "\033[1;32m╭───────────────────────────────╮\033[0m"
