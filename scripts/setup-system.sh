ee11292592ce59e2f59c499651b59c57195cfb61
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2025-06-01 19:46:12 -0700

    user setup

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -4,10 +4,36 @@ set -e
 echo "Enter a password for root user:"
 sudo passwd root
 
-echo "Enter a password for devpod-user:"
-sudo passwd devpod-user 
+echo "Enter a password for user vscode:"
+sudo passwd vscode
+
+DEVPOD_USER="devpod-user"
+if id -u vscode >/dev/null 2>&1; then
+  echo "Renaming user 'vscode' to '$DEVPOD_USER'..."
+  sudo usermod -l "$DEVPOD_USER" vscode
+  sudo groupmod -n "$DEVPOD_USER" vscode
+
+  echo "Moving home directory to /home/$DEVPOD_USER ..."
+  sudo usermod -d "/home/$DEVPOD_USER" -m "$DEVPOD_USER"
+
+	echo "Enter a password for user '$DEVPOD_USER:"
+	sudo passwd vscode
+
+  echo "Adding '$DEVPOD_USER' to sudo group..."
+  sudo usermod -aG sudo "$DEVPOD_USER"
+
+  echo "Changing default shell for $DEVPOD_USER to zsh..."
+	sudo chsh -s "$(which zsh)" "$(whoami)"
+fi
 
 # setup zsh shell
-echo "Setting up zshrc and switching to zsh..."
-sudo chsh -s "$(which zsh)" "$(whoami)"
+echo "Setting up zshrc..."
 zsh -c 'source "$HOME/dotfiles/.zshrc"'
+
+echo -e "\n\n"
+echo -e "\033[1;32m╭───────────────────────────────╮\033[0m"
+echo -e "\033[1;32m│                               │\033[0m"
+echo -e "\033[1;32m│    System setup complete!     │\033[0m"
+echo -e "\033[1;32m│                               │\033[0m"
+echo -e "\033[1;32m╰───────────────────────────────╯\033[0m"
+echo -e "\n\n"
